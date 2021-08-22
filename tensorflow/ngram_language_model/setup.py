73e3cab698067636076f98ca7439a03bd396a1ff
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-22 20:53:03 +0800

    Delete tensorflow directory

--- tensorflow/ngram_language_model/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-# run python3 setup.py build_ext --inplace
-setup(
-  name = 'ngram language model',
-  ext_modules = cythonize("ngram_language_model.pyx", language="c++"),
-)
